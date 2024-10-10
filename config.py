2208e0c30a4303d0fd5a9c9c7bbd2f5663e6f505
Yassine Moussafir <yassinemoussafir31@gmail.com>
2024-10-10 17:22:10 +0100

    rm config file

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Meriem60641939'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://geosnap_user:Meriem60641939@localhost/geosnap_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_PERMANENT = False
